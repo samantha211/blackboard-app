197e0334a442c6410679f067e57110eda7e9314c
samanthalloyds <samantha.c.lloyds@gmail.com>
2020-11-24 11:13:35 -0500

    deploy to heroku

--- server.js
+++ server.js
@@ -8,4 +8,4 @@ app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+'/dist/routing-demo-class4/index.html'));
 });
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 8080);
